03f5014b9ae76b858887767607543b95b734c87d
Nancy <nevelasquez113@gmail.com>
2018-01-30 13:59:27 -0500

    Format: Removed semicolons

--- app/components/Calendar.jsx
+++ app/components/Calendar.jsx
@@ -1,21 +1,21 @@
 
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import moment from 'moment'
 import { connect } from 'react-redux'
-import { dateEntry } from '../reducers/entry';
+import { dateEntry } from '../reducers/entry'
 
-import 'react-dates/initialize';
-import 'react-dates/lib/css/_datepicker.css';
+import 'react-dates/initialize'
+import 'react-dates/lib/css/_datepicker.css'
 
-import { SingleDatePicker } from 'react-dates';
+import { SingleDatePicker } from 'react-dates'
 
 class Calendar extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       date: null,
       focused: null,
-    };
+    }
   }
 
   render() {
@@ -30,12 +30,12 @@ class Calendar extends Component {
           showClearDate={true}
           date={this.state.date}
           focused={this.state.focused}
-          onDateChange={(date) => { this.setState({ date }); }}
+          onDateChange={(date) => { this.setState({ date }) }}
           onFocusChange={({ focused }) => { this.setState({ focused }) }}
           isOutsideRange={() => false}
         />
       </div>
-    );
+    )
   }
 }
 
